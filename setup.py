a47fc1ee370321540d61ddd76decf3a0d57291c1
Asko Soukka <asko.soukka@iki.fi>
2012-10-14 22:33:44 +0300

    Prepare sphinxcontrib-robotdoc 0.1.1.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sphinxcontrib-robotdoc",
-    version="0.1.0",
+    version="0.1.1",
     description="",
     long_description=(open("README.rst").read() + "\n" +
                       open("CHANGES.txt").read()),
