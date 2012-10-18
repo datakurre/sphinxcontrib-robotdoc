55f6c497e7dd3d09c4293f031403d72f32b20046
Asko Soukka <asko.soukka@iki.fi>
2012-10-18 06:35:11 +0300

    Added support for 'tags'. Added alternative 'source' as an alias for 'suite' option. Fixed to filter comment lines from steps.

--- src/sphinxcontrib_robotdoc/__init__.py
+++ src/sphinxcontrib_robotdoc/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-""" Robot Framework AutoDoc for Sphinx """
+"""Robot Framework AutoDoc for Sphinx"""
 
 import re
 import os
@@ -30,11 +30,15 @@ def TestCaseNode(obj):
     doc = obj.doc.value.replace('\\n', '\n')  # fix linebreaks
     test.append(nodes.paragraph(text=doc))
 
+    comment = re.compile("^\s*#.*")
+    all_steps = filter(lambda x: not comment.match(x.as_list()[0]), obj.steps)
+
     steps = nodes.literal_block()
-    steps.extend(map(StepNode, obj.steps))
-    # insert newlines:
-    for i in range(len(obj.steps[:-1]), 0, -1):
-        steps.insert(i, nodes.inline(text="\n"))
+    steps.extend(map(StepNode, all_steps))
+
+    # Insert newlines between steps:
+    for i in range(len(all_steps[:-1]), 0, -1):
+        steps.insert(i, nodes.inline(text='\n'))
     test.append(steps)
 
     return test
@@ -51,12 +55,15 @@ def KeywordNode(obj):
     doc = obj.doc.value.replace('\\n', '\n')  # fix linebreaks
     test.append(nodes.paragraph(text=doc))
 
+    comment = re.compile("^\s*#.*")
+    all_steps = filter(lambda x: not comment.match(x.as_list()[0]), obj.steps)
+
     steps = nodes.literal_block()
-    steps.extend(map(StepNode, obj.steps))
+    steps.extend(map(StepNode, all_steps))
 
     # insert newlines:
-    for i in range(len(obj.steps[:-1]), 0, -1):
-        steps.insert(i, nodes.inline(text="\n"))
+    for i in range(len(all_steps[:-1]), 0, -1):
+        steps.insert(i, nodes.inline(text='\n'))
 
     test.append(steps)
 
@@ -64,53 +71,70 @@ def KeywordNode(obj):
 
 
 class TestCasesDirective(Directive):
-    """ Robot Directive """
+    """Robot test cases directive"""
 
     has_content = True
 
     option_spec = {
-        "suite": directives.path,
+        'source': directives.path,
+        'suite': directives.path,  # alias for 'source'
+        'tags': directives.unchanged,
     }
 
     def run(self):
         source_directory = os.path.dirname(self.state.document.current_source)
         to_cwd = os.path.relpath(os.getcwd(), source_directory)
 
-        filename = os.path.relpath(self.options["suite"], to_cwd)
+        path = self.options.get('source', self.options.get('suite'))
+        filename = os.path.relpath(path, to_cwd)
         suite = robot.parsing.TestData(source=filename)
 
         if self.content:
             needle = re.compile(self.content[0].strip(), re.U)
         else:
-            needle = re.compile(".*", re.U)
+            needle = re.compile('.*', re.U)
 
         tests = []
         suite_parent = os.path.dirname(filename)
 
+        # 1) Recurse through the test suite and filter tests by name
         def recurse(child_suite, suite_parent):
             tests.extend(filter(lambda x: needle.match(x.name),
                          child_suite.testcase_table.tests))
+
             for grandchild in getattr(child_suite, 'children', []):
                 recurse(grandchild, suite_parent)
         recurse(suite, suite_parent)
 
+        # 2) When tags option is set, filter the found tests by given tags
+        tags = map(lambda x: x.strip(),
+                   self.options.get('tags', '').split(','))
+        tag_filter = lambda x: filter(lambda y: bool(y),
+                                      [tag in x.tags.value for tag in tags])
+        tests = filter(lambda x: tag_filter(x), tests) if tags else tests
+
+        # Finally, return Docutils nodes for the tests
         return map(TestCaseNode, tests)
 
 
 class KeywordsDirective(Directive):
-    """ Robot Directive """
+    """Robot user keywords directive"""
 
     has_content = True
 
     option_spec = {
-        "suite": directives.path,
+        'source': directives.path,
+        'suite': directives.path,  # alias for 'source'
+        'resource': directives.path,  # alias for 'source'
     }
 
     def run(self):
         source_directory = os.path.dirname(self.state.document.current_source)
         to_cwd = os.path.relpath(os.getcwd(), source_directory)
 
-        filename = os.path.relpath(self.options["suite"], to_cwd)
+        path = self.options.get(
+            'source', self.options.get('suite', self.options.get('resource')))
+        filename = os.path.relpath(path, to_cwd)
         try:
             resource = robot.parsing.TestData(source=filename)
         except robot.errors.DataError:
@@ -120,7 +144,7 @@ class KeywordsDirective(Directive):
         if self.content:
             needle = re.compile(self.content[0].strip(), re.U)
         else:
-            needle = re.compile(".*", re.U)
+            needle = re.compile('.*', re.U)
 
         keywords = filter(lambda x: needle.match(x.name),
                           resource.keywords)
