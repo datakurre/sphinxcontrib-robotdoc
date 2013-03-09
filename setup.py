00b2bda2c3b5d375120caac040393be45fab8dec
Asko Soukka <asko.soukka@iki.fi>
2013-03-09 17:02:18 +0200

    Preparing release 0.4.0-rc.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sphinxcontrib-robotdoc",
-    version="0.3.4",
+    version="0.4.0-rc.1",
     description=("Sphinx extension to embed Robot Framework test cases "
                  "and and user keywords into Sphinx documents"),
     long_description=(open("README.rst").read() + "\n" +
