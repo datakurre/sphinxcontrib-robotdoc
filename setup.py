f35c2f5ede43705c3c91b37dee20b9666f48f186
Asko Soukka <asko.soukka@iki.fi>
2019-11-08 15:03:47 +0200

    Back to development: 0.11.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sphinxcontrib-robotdoc',
-    version='0.11.0',
+    version='0.11.1.dev0',
     description=('Sphinx extension to embed Robot Framework test cases '
                  'and user keywords into Sphinx documents'),
     long_description=(open('README.rst').read() + '\n' +
